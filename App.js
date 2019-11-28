d614a9e0da930effb5565f59b977a3f0fd15fd49
George Mantellos <geocfuone@gmail.com>
2019-11-28 19:15:47 +0200

    initial app

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { RecordSensors } from './src/Screens/RecordSensors';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <RecordSensors />
+    );
+  }
+}
